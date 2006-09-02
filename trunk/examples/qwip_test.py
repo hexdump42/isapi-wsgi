c18a2a736924d1f0541d0b65acfc9ff5f6ee5ec3
mark.john.rees <mark.john.rees@926716e8-761c-0410-85d1-e31c6663a17e>
2006-09-02 12:03:46 +0000

    Initial checkin as at Rev 17 of isapi-wsgi.python-hosting.com

---
+++ trunk/examples/qwip_test.py
@@ -0,0 +1,44 @@
+# Demo of running a quixote app using Titus Browns' QWIP under isapi-wsgi
+#
+# Executing this script (or any server config script) will install the extension
+# into your web server and will create a "loader" DLL _qwip_test.dll in the 
+# current directory. As the server executes, the PyISAPI framework will load
+# this module and create the Extension object.
+# A Virtual Directory named "isapi-wsgi-qwip-test" is setup. This dir has the ISAPI
+# WSGI extension as the only application, mapped to file-extension '*'.  
+# Therefore, isapi_wsgi extension handles *all* requests in this directory.
+#
+# Requires 
+#  * Quixote 1.2 (http://www.mems-exchange.org/software/quixote/)
+#  * QWIP (http://www.idyll.org/~t/www-tools/wsgi/)
+#
+# To launch this application from a web browser use a url similar to:
+#
+#  http://localhost/isapi-wsgi-qwip-test/demo
+#
+
+import isapi_wsgi
+import qwip
+
+# The entry points for the ISAPI extension.
+def __ExtensionFactory__():
+    return isapi_wsgi.ISAPISimpleHandler(demo = qwip.QWIP('quixote.demo'))
+
+if __name__=='__main__':
+    # If run from the command-line, install ourselves.
+    from isapi.install import *
+    params = ISAPIParameters()
+    # Setup the virtual directories - this is a list of directories our
+    # extension uses - in this case only 1.
+    # Each extension has a "script map" - this is the mapping of ISAPI
+    # extensions.
+    sm = [
+        ScriptMapParams(Extension="*", Flags=0)
+    ]
+    vd = VirtualDirParameters(Name="isapi-wsgi-qwip-test",
+                              Description = "ISAPI-WSGI QWIP Test",
+                              ScriptMaps = sm,
+                              ScriptMapUpdate = "replace"
+                              )
+    params.VirtualDirs = [vd]
+    HandleCommandLine(params)
---
+++ trunk/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name='isapi_wsgi',
+	version='0.9',
+	description='A WSGI handler for ISAPI',
+	author='Mark Rees',
+	author_email='hex-dump at hotmail dot com',
+	url = "isapi_wsgi.python-hosting.com",
+	license='MIT',
+	py_modules=['isapi_wsgi'],
+	packages=['tests']
+	)
