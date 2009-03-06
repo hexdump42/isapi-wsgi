06456ce5a1e08b5d4d9b60fd606f3ee827a9bfde
jaraco@jaraco.com <jaraco@jaraco.com@926716e8-761c-0410-85d1-e31c6663a17e>
2009-03-06 15:20:44 +0000

    Integrated branches/scriptname-handling-fix into the trunk - SCRIPT_NAME and PATH_INFO are now more robustly calculated.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,13 +1,35 @@
 
 from distutils.core import setup
 
+try:
+	from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+	from distutils.command.build_py import build_py
+
+try:
+	from distutils.command.build_scripts import build_scripts_2to3 as build_scripts
+except ImportError:
+	from distutils.command.build_scripts import build_scripts
+
+# Load the reStructuredText document from docs
+import os
+this_dir = os.path.dirname(__file__)
+doc_file = os.path.join(this_dir, 'docs', 'ABOUT.txt')
+long_description = open(doc_file).read()
+
+# PyPI will use rst2html from docutils to convert ABOUT.txt to HTML
+
 setup(name='isapi_wsgi',
 	version='0.3',
 	description='A WSGI handler for ISAPI',
+	long_description=long_description,
 	author='Mark Rees',
 	author_email='mark dot john dot rees at gmail dot com',
 	url = "code.google.com/p/isapi-wsgi",
 	license='MIT',
 	py_modules=['isapi_wsgi'],
-	packages=['tests']
+	packages=['tests'],
+	cmdclass = {'build_py':build_py,
+	            'build_scripts':build_scripts,
+	           }
 	)
